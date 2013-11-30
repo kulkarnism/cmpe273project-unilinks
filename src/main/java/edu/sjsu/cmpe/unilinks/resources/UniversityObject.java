ce391e5ae8d6f20f1c35f099e374f3ac3fa2c9de
anitajebaraj <anita.tvl@gmail.com>
2013-11-29 18:08:20 -0800

    Integrated Code

--- src/main/java/edu/sjsu/cmpe/unilinks/resources/UniversityObject.java
+++ src/main/java/edu/sjsu/cmpe/unilinks/resources/UniversityObject.java
@@ -1,133 +1,73 @@
 package edu.sjsu.cmpe.unilinks.resources;
-
-
+import java.util.ArrayList;
+import org.codehaus.jackson.annotate.JsonProperty;
 
 public class UniversityObject {
-	private String schoolName,tuitionFees,location,contact,department;
-	private String springApplnDate,fallApplnDate;
-	private long greScore,toeflScore;
-	double ieltsScore;
-
-	public UniversityObject()
-	{
-		
-	}
-
-	public UniversityObject(String schoolName, String tuitionFees,
-			String location, String contact, String department,
-			String springApplnDate, String fallApplnDate, long greScore,
-			long toeflScore, long ieltsScore) {
-		super();
-		this.schoolName = schoolName;
-		this.tuitionFees = tuitionFees;
-		this.location = location;
-		this.contact = contact;
-		this.department = department;
-		this.springApplnDate = springApplnDate;
-		this.fallApplnDate = fallApplnDate;
-		this.greScore = greScore;
-		this.toeflScore = toeflScore;
-		this.ieltsScore = ieltsScore;
-	}
-
-
-	public String getSchoolName() {
-		return schoolName;
-	}
-
-
-	public void setSchoolName(String schoolName) {
-		this.schoolName = schoolName;
-	}
-
-
-	public String getTuitionFees() {
-		return tuitionFees;
-	}
-
-
-	public void setTuitionFees(String tuitionFees) {
-		this.tuitionFees = tuitionFees;
-	}
-
-
-	public String getLocation() {
-		return location;
-	}
-
-
-	public void setLocation(String location) {
-		this.location = location;
-	}
-
-
-	public String getContact() {
-		return contact;
-	}
-
-
-	public void setContact(String contact) {
-		this.contact = contact;
-	}
-
-
-	public String getDepartment() {
-		return department;
-	}
-
-
-	public void setDepartment(String department) {
-		this.department = department;
-	}
-
-
-	public String getSpringApplnDate() {
-		return springApplnDate;
-	}
-
-
-	public void setSpringApplnDate(String springApplnDate) {
-		this.springApplnDate = springApplnDate;
-	}
-
-
-	public String getFallApplnDate() {
-		return fallApplnDate;
-	}
-
-
-	public void setFallApplnDate(String fallApplnDate) {
-		this.fallApplnDate = fallApplnDate;
-	}
-
-
-	public long getGreScore() {
-		return greScore;
-	}
-
-
-	public void setGreScore(long greScore) {
-		this.greScore = greScore;
-	}
-
-
-	public long getToeflScore() {
-		return toeflScore;
-	}
-
-
-	public void setToeflScore(long toeflScore) {
-		this.toeflScore = toeflScore;
-	}
-
-
-	public double getIeltsScore() {
-		return ieltsScore;
-	}
-
-
-	public void setIeltsScore(double d) {
-		this.ieltsScore = d;
-	}
+    @JsonProperty("_id")
+    int _id = 0;    
+    @JsonProperty("SchoolName")
+    String SchoolName;
+    @JsonProperty("department")
+    ArrayList<DepartmentObject> department = new ArrayList<DepartmentObject>();
+    @JsonProperty("location")
+    String location;
+    @JsonProperty("tutionFees")
+    int tutionFees = 0;
+    @JsonProperty("FallApplnDate")
+    String FallApplnDate;
+    @JsonProperty("SpringApplnDate")
+    String SpringApplnDate;
+    @JsonProperty("contactInfo")
+    String contactInfo;
+    
+    public int getId() {
+    return _id;
+    }
+    public void setId(int id) {
+    this._id = id;
+    }
+    
+    public int getTutionFees() {
+    return tutionFees;
+    }
+    public void setTutionFees(int tutionFees) {
+    this.tutionFees = tutionFees;
+    }
+    public String getFallApplnDate() {
+    return FallApplnDate;
+    }
+    public void setFallApplnDate(String fallApplnDate) {
+    FallApplnDate = fallApplnDate;
+    }
+    public String getSpringApplnDate() {
+    return SpringApplnDate;
+    }
+    public void setSpringApplnDate(String springApplnDate) {
+    SpringApplnDate = springApplnDate;
+    }
+    public String getContactInfo() {
+    return contactInfo;
+    }
+    public void setContactInfo(String contactInfo) {
+    this.contactInfo = contactInfo;
+    }
+    public ArrayList<DepartmentObject> getDepartment() {
+    return department;
+    }
+    public void setDepartment(ArrayList<DepartmentObject> department) {
+    this.department = department;
+    }
+    public String getSchoolName() {
+    return SchoolName;
+    }
+    public void setSchoolName(String SchoolName) {
+    this.SchoolName = SchoolName;
+    }
+    public String getLocation() {
+    return location;
+    }
+    public void setLocation(String location) {
+    this.location = location;
+    }
 
 }
